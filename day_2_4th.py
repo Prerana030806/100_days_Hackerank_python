6c1b17124a7c0d3f61c0de52627d3c9afd59f1f4
Prerana030806 <preranashinde317@gmail.com>
2026-01-11 21:41:02 +0530

    Create day_2_4th.py
    4th python problem using if else statement to check the year is laep or not using boolean True or False on hackerank day 2

---
+++ day_2_4th.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    # Write your logic here
+      return True
+    else :
+      return False 
+
+year = int(input())
+print(is_leap(year))
