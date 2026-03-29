fa6a7a4d6c0df5d2e56af5cbc8ab7e789aee6155
Prerana030806 <preranashinde317@gmail.com>
2026-03-29 20:18:35 +0530

    Create 1st.py
    Hackerank day 79 .

---
+++ day_79.py/1st.py
@@ -0,0 +1,12 @@
+import re
+import email.utils
+
+n = int(input())
+
+pattern = r'^[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z]+\.[a-zA-Z]{1,3}$'
+
+for _ in range(n):
+    name, addr = email.utils.parseaddr(input())
+    
+    if re.match(pattern, addr):
+        print(email.utils.formataddr((name, addr)))
