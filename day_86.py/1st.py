f93c5182b440a4491f5caf5a2587aa74410afebd
Prerana030806 <preranashinde317@gmail.com>
2026-04-05 23:46:54 +0530

    Add probability calculation for combinations containing 'a'
    Hackerank day 86 .

---
+++ day_86.py/1st.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+n = int(input())
+letters = input().split()
+k = int(input())
+all_combinations = list(combinations(letters, k))
+count = 0
+for comb in all_combinations:
+    if 'a' in comb:
+        count += 1
+probability = count / len(all_combinations)
+print(probability)
